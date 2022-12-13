1661eca6f8aa4b10bb8138c82bd004467d4e8bc1
jigivic <jigivic@gmail.com>
2022-12-12 18:24:52 -0800

    combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - to print all possible combinations of a single-digit number
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar ((num % 10) + '0');
+	
+	if (num == 9)
+	
+		continue;
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
+
