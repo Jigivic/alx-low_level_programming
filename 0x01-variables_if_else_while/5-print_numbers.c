7da32b02dba1eaaca45bcbc81af5a518ada12dfc
jigivic <jigivic@gmail.com>
2022-12-12 17:19:33 -0800

    print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - to print all sungle digit to number base ten
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d" , n);
+	}
+	printf("\n");
+	return (0);
+}
