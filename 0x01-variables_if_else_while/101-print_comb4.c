198a3a5cd05b15f4bc02d5575ecc4e3074b27bbb
jigivic <jigivic@gmail.com>
2022-12-12 19:26:33 -0800

    combo4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - print all possible combination of three different digit,in ascending order, separated by a comma followed by a space.	
+ * main - print all combination in ascending/decending order.	
  *
  * Return: Always 0 (success)
  */
@@ -21,14 +21,14 @@ int main(void)
 			{
 				putchar((digit1 % 10) + '0');
 				putchar((digit2 % 10) + '0');
-				putchar(( digit3 % 10) + '0');
+				putchar((digit3 % 10) + '0');
 
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					continue;
 				putchar(',');
 				putchar(' ');
 			}
-		}	
+		}
 	}
 	putchar('\n');
 	return (0);
