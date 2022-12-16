503f68f2becbfd201be080ca203c9cf1a599ec0a
jigivic <jigivic@gmail.com>
2022-12-16 15:17:30 -0800

    task7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,19 +8,24 @@
  */
 void print_diagonal(int n)
 {
-	int i, j;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
-	for (i = 0; i < n; i++)
+	else
 	{
-		for (j = 0; j < i; j++)
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\')
+				else if (j < i)
+					_putchar('');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
-		_putchar('\\');
 	}
 }
